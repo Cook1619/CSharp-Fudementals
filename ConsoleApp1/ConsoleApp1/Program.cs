e54fcde92bc2bf176fc4f811e5676cb208bdc761
Cook1619 <cook6348@gmail.com>
2018-09-10 12:05:05 -0500

    Adding more functionality

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,42 +10,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // TODO Declare a boolean variable named "keepGoing"
-            // and assign it a value of "true". Use this variable to 
-            // know when to exit the while loop.
-            bool keepGoing = true;
-            Console.Write("Is this working?");
-            // TODO Define a while loop.
-            // Keep looping as long as the variable "keepGoing" has a value of "true".
-            while (keepGoing)
-            {
-                // TODO Prompt the user with the text "Enter a number:" 
-                Console.Write("Pick a number: ");
-                string entry = Console.ReadLine();
-                // and assign their value to an "entry" string variable.
+           
+                int runningTotal = 0;
+                bool keepGoing = true;
 
-                // TODO If the user entered the text "quit"...
-                if (entry == "quit")
+                while (keepGoing)
                 {
-                    // TODO Else the user didn't enter the text "quit"...
-                    keepGoing = false;
+                    //Prompt user for minutes exercised
+                    Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                    string entry = Console.ReadLine();
+
+                    if (entry == "quit")
+                    {
+                        keepGoing = false;
+                    }
+                    else
+                    {
+                        int minutes = int.Parse(entry);
+
+                        if (minutes <= 10)
+                        {
+                            Console.WriteLine("At least you're trying");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Wow, nice work out");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You're a ninja!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Now you're just showing off!!");
+                        }
+                        runningTotal = runningTotal + minutes;
+
+                        //Add minutes exercised to a total
+                        //Display total minutes excersied to the screen
+                        Console.WriteLine("You've exercised " + runningTotal + " minutes.");
+                        //Repeat until user quits
+                    }
                 }
-                else
-                {
-                    // TODO Parse the user's entry to an integer
-                    int number = int.Parse(entry);
-                    // TODO Square the user's provided number 
-                    // (i.e. multiply the number by itself).
-                    int result = number * number;
-                    // TODO Output the result.
-                    Console.WriteLine("The square of " + number + " is " + result);
-                    // Example: "The square of 2 is 4." or 
-                    // "2 multiplied by itself is equal to 4."
-                }
+                Console.WriteLine("Goodbye");
             }
-            // TODO Output the text "Goodbye!" after exiting the loop.
-            Console.WriteLine("GoodBye!!!");
-            
         }
-    }
 }
